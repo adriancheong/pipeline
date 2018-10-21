b025baec05a18a4280142d0cf11ff65470a55968
adriancheong <adriancheong@gic.com.sg>
2018-10-21 16:36:20 +0700

    Update Engine.groovy

--- src/ac/globals/Engine.groovy
+++ src/ac/globals/Engine.groovy
@@ -3,9 +3,14 @@ package ac.globals
 import groovy.json.JsonSlurperClassic
 
 class Engine implements Serializable {
+  def script
+  Engine(script) {
+    this.script = script
+  }
+  
   def hello(){
     println 'hello'
-    sh 'echo hello'
+    script.sh 'echo hello'
     bat 'echo hello'
     return 'Hello World!'
   }
